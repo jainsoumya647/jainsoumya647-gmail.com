ed8f12a534def29aae76c303189616d9479c8d65
Soumya <jainsoumya647@gmail.com>
2020-03-30 00:08:14 +0530

    Initial Project added

---
+++ McKinley&Rice/AppGroup/View/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  McKinley&Rice
+//
+//  Created by Soumya Jain on 30/03/20.
+//  Copyright © 2020 Soumya Jain. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
